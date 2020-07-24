559c3baa8e0aa1325d7c08cfbac8fe19c4c6c7dd
dnor-dev <anorue4rss@hotmail.com>
2020-07-23 20:53:16 -0700

    remote version ready

---
+++ JS/app.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.hero-text', {
+    strings: ['Start to finish illustration course from world famous author Jane Doe.'],
+    smartBackspace: false,
+    typeSpeed: 100, // Default value
+    fadeOut: true,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 900,
+    showCursor: false
+  });
